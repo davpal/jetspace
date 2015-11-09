8dab930dbfbe81308eb8825b99c52f7f71267686
davpal <paluchdaw@gmail.com>
2015-11-09 20:35:59 +0100

    Update formatting in Renderer

--- src/rendering/Renderer.java
+++ src/rendering/Renderer.java
@@ -1,4 +1,5 @@
 package rendering;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -9,43 +10,46 @@ import entity.Weapon;
 import resource.ResourceFactory;
 
 public class Renderer {
-	private Image playerShip = ResourceFactory.getPlayerShip();
-	private Animation explosion = ResourceFactory.getExplosion();
-	
-	private boolean explosionRunning;
-	
-	public Renderer(){
 
-	}
-	
-	public void render(Graphics g, Player player){	
-        if(!player.isCrashing()){
+    private Image playerShip = ResourceFactory.getPlayerShip();
+    private Animation explosion = ResourceFactory.getExplosion();
+
+    private boolean explosionRunning;
+
+    public Renderer() {
+
+    }
+
+    public void render(Graphics g, Player player) {
+        if (!player.isCrashing()) {
             g.pushTransform();
-            g.rotate((float)player.getCenterX(), (float)player.getCenterY(),
-            		 (float)Math.toDegrees(player.getAngle()));
-            g.drawImage(playerShip, (float)player.getX(), (float)player.getY());
+            g.rotate((float) player.getCenterX(), (float) player.getCenterY(),
+                    (float) Math.toDegrees(player.getAngle()));
+            g.drawImage(playerShip, (float) player.getX(), (float) player.getY());
             g.popTransform();
             g.pushTransform();
             g.setColor(new Color((10 - player.getHealth()) * 25, player.getHealth() * 25, 0, 220));
-            g.fillRect((int)(player.getX() + (player.getWidth() - 50) / 2), 
-            		   (int)(player.getY() + player.getHeight() - 2),
-            		   5 * player.getHealth(), 5);
+            g.fillRect((int) (player.getX() + (player.getWidth() - 50) / 2),
+                    (int) (player.getY() + player.getHeight() - 2),
+                    5 * player.getHealth(), 5);
             g.popTransform();
-        }
-        else {
-        	if(explosionRunning){
-        		explosion.start();
-        		explosionRunning = true;
-        	}
-        	explosion.draw((float)player.getX(), (float)player.getY());
-        	if(explosion.isStopped()) player.kill();
+        } else {
+            if (explosionRunning) {
+                explosion.start();
+                explosionRunning = true;
+            }
+            explosion.draw((float) player.getX(), (float) player.getY());
+            if (explosion.isStopped()) {
+                player.kill();
+            }
         }
 
-        for(Weapon w : player.getWeapons())
+        for (Weapon w : player.getWeapons()) {
             w.render(g);
-	}
-	
-	public void render(Weapon w){
-		
-	}
+        }
+    }
+
+    public void render(Weapon w) {
+
+    }
 }
