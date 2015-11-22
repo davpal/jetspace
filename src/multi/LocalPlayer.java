65f24c60f61ee0c0c2a5ae5739c236ede7599d39
winio94 <winio94@gmail.com>
2015-11-22 17:45:16 +0100

    add multiplayers

---
+++ src/multi/LocalPlayer.java
@@ -0,0 +1,22 @@
+package multi;
+
+
+import entity.Player;
+import org.newdawn.slick.GameContainer;
+import rendering.Renderer;
+
+public class LocalPlayer extends Player {
+    protected LocalPlayer(double d, double e, double a) {
+        super(d, e, a);
+    }
+
+    @Override
+    public void update(GameContainer g) {
+
+    }
+
+    @Override
+    public void render(Renderer r) {
+
+    }
+}
---
+++ src/multi/NetworkPlayer.java
@@ -0,0 +1,21 @@
+package multi;
+
+import entity.Player;
+import org.newdawn.slick.GameContainer;
+import rendering.Renderer;
+
+public class NetworkPlayer extends Player {
+    protected NetworkPlayer(double d, double e, double a) {
+        super(d, e, a);
+    }
+
+    @Override
+    public void update(GameContainer g) {
+
+    }
+
+    @Override
+    public void render(Renderer r) {
+
+    }
+}
