b67a25c3ec5d8bcfa53032479b5d5a00dfe748b2
davpal <paluchdaw@gmail.com>
2015-11-30 19:20:05 +0000

    Add player socket

---
+++ src/multi/PlayerSocket.java
@@ -0,0 +1,38 @@
+package multi;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class PlayerSocket {
+    public static final int PACKET_SIZE = 20;
+    DatagramSocket socket;
+
+    PlayerSocket(int port, InetAddress address) throws Exception {
+        socket = new DatagramSocket(port, address);
+    }
+
+    public void send(DatagramPacket p) {
+        try {
+            socket.send(p);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public DatagramPacket receive() {
+        byte[] data = new byte[PlayerSocket.PACKET_SIZE];
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+        try {
+            socket.receive(packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return packet;
+    }
+
+    public void close() {
+        socket.close();
+    }
+}
