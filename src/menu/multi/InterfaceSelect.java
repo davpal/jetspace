39fd341bf38f953ca1e460b4e09c32818da4f6cd
davpal <paluchdaw@gmail.com>
2015-12-09 17:01:25 +0000

    Add InterfaceSelect component

--- src/menu/multi/InterfaceSelect.java
+++ src/menu/multi/InterfaceSelect.java
@@ -1,31 +1,45 @@
 package menu.multi;
 
 import java.net.InetAddress;
-import menu.MenuItem;
-import org.newdawn.slick.state.StateBasedGame;
+import java.util.ArrayList;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.gui.BasicComponent;
+import org.newdawn.slick.gui.GUIContext;
 import rendering.Renderer;
 
-public class InterfaceSelect extends MenuItem {
-    InetAddress address;
+public class InterfaceSelect extends BasicComponent {
+    ArrayList<InetAddress> addresses;
+    int selected;
 
-    public InterfaceSelect(InetAddress a){
-        super(a.toString());
-        address = a;
+    public InterfaceSelect(GUIContext gc, ArrayList<InetAddress> addresses){
+        super(gc);
+        this.addresses = addresses;
+        this.selected = 0;
     }
 
     public InetAddress getAddress() {
-        return address;
+        return addresses.get(selected);
     }
 
     public String toString(){
-        return address.toString();
+        return addresses.get(selected).toString();
     }
 
-    public void execute(StateBasedGame g) {
-        g.enterState(3);
+    public void prev(){
+        --selected;
+        if(selected < 0) selected = addresses.size() - 1;
+    }
+
+    public void next(){
+        ++selected;
+        if(selected > addresses.size() - 1) selected = 0;
     }
 
     public void render(Renderer r){
         r.renderInterfaceSelect(this);
+    }
+
+    @Override
+    public void renderImpl(GUIContext gc, Graphics g) {
     }
 }
