46b19856f7673afc370924242a205ae351ed8d7d
davpal <paluchdaw@gmail.com>
2016-01-09 18:05:33 +0000

    Add packet sender thread

--- src/multi/LocalPlayerListener.java
+++ src/multi/LocalPlayerListener.java
@@ -5,10 +5,11 @@ import input.PlayerInputListener;
 
 public class LocalPlayerListener extends PlayerInputListener {
     PlayerSocket socket;
+    PacketSender sender;
 
     public LocalPlayerListener(Player player, PlayerSocket socket){
         super(player);
-        this.socket = socket;
+        sender = new PacketSender(socket);
     }
 
     LocalPlayerListener() {
@@ -17,21 +18,21 @@ public class LocalPlayerListener extends PlayerInputListener {
 
     public void keyPressed(int key, char arg1) {
         super.keyPressed(key, arg1);
-        socket.send(new Message(Message.MOVE, (int) player.getX(), (int) player.getY(), player.getAngle()));
+        sender.add(new Message(Message.MOVE, (int) player.getX(), (int) player.getY(), player.getAngle()));
     }
 
     public void keyReleased(int key, char arg1) {
         super.keyReleased(key, arg1);
-        socket.send(new Message(Message.STOP, (int) player.getX(), (int) player.getY(), player.getAngle()));
+        sender.add(new Message(Message.STOP, (int) player.getX(), (int) player.getY(), player.getAngle()));
     }
 
     public void mouseMoved(int arg0, int arg1, int mx, int my) {
         super.mouseMoved(arg0, arg1, mx, my);
-        socket.send(new Message(Message.MOVE, (int) player.getX(), (int) player.getY(), player.getAngle()));
+        sender.add(new Message(Message.MOVE, (int) player.getX(), (int) player.getY(), player.getAngle()));
     }
 
     public void mouseClicked(int button, int arg1, int arg2, int arg3) {
         super.mouseClicked(button, arg1, arg2, arg3);
-        socket.send(new Message(Message.SHOOT, (int) player.getX(), (int) player.getY(), player.getAngle()));
+        sender.add(new Message(Message.SHOOT, (int) player.getX(), (int) player.getY(), player.getAngle()));
     }
 }
---
+++ src/multi/PacketSender.java
@@ -0,0 +1,29 @@
+package multi;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class PacketSender implements Runnable {
+    private PlayerSocket socket;
+    private boolean running;
+    private Deque<Message> messages = new LinkedList<>();
+
+    public PacketSender(PlayerSocket socket) {
+        this.socket = socket;
+        running = true;
+        new Thread(this).start();
+    }
+
+    public synchronized void add(Message m){
+        messages.add(m);
+    }
+
+    @Override
+    public void run(){
+        while(running) {
+            while(!messages.isEmpty()){
+                socket.send(messages.poll());
+            }
+        }
+    }
+}
