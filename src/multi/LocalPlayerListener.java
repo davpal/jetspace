db03be2c11a0a6038a33210821fb05259a6519a3
davpal <paluchdaw@gmail.com>
2016-01-16 21:35:40 +0100

    Fix multiple key press in multiplayer mode

--- src/multi/LocalPlayerListener.java
+++ src/multi/LocalPlayerListener.java
@@ -2,11 +2,13 @@ package multi;
 
 import entity.Player;
 import input.PlayerInputListener;
+import java.util.ArrayList;
 import org.newdawn.slick.Input;
 
 public class LocalPlayerListener extends PlayerInputListener {
     PlayerSocket socket;
     PacketSender sender;
+    private ArrayList<Integer> pressedKeys = new ArrayList<>();
 
     public LocalPlayerListener(Player player, PacketSender sender){
         super(player);
@@ -19,32 +21,30 @@ public class LocalPlayerListener extends PlayerInputListener {
 
     public void keyPressed(int key, char arg1) {
         super.keyPressed(key, arg1);
+        pressedKeys.add(key);
 
-        double dx, dy;
-
-        switch (key) {
-            case Input.KEY_D: {
-                dx = player.getSpeed();
-                dy = 0;
-                break;
+        double dx = 0, dy = 0;
+        for(int k : pressedKeys) {
+            switch (k) {
+                case Input.KEY_D: {
+                    dx = player.getSpeed();
+                    break;
+                }
+                case Input.KEY_A: {
+                    dx = -player.getSpeed();
+                    break;
+                }
+                case Input.KEY_W: {
+                    dy = -player.getSpeed();
+                    break;
+                }
+                case Input.KEY_S: {
+                    dy = player.getSpeed();
+                    break;
+                }
+                default:
+                    dx = dy = 0;
             }
-            case Input.KEY_A: {
-                dx = -player.getSpeed();
-                dy = 0;
-                break;
-            }
-            case Input.KEY_W: {
-                dx = 0;
-                dy = -player.getSpeed();
-                break;
-            }
-            case Input.KEY_S: {
-                dx = 0;
-                dy = player.getSpeed();
-                break;
-            }
-            default:
-                dx = dy = 0;
         }
 
         sender.add(new Message(Message.MOVE, (int)dx, (int)dy, player.getAngle()));
@@ -52,6 +52,7 @@ public class LocalPlayerListener extends PlayerInputListener {
 
     public void keyReleased(int key, char arg1) {
         super.keyReleased(key, arg1);
+        pressedKeys.remove(Integer.valueOf(key));
         sender.add(new Message(Message.STOP, (int) player.getX(), (int) player.getY(), player.getAngle()));
     }
 
